a37515c09f1f54a5b259ec14eddb4351f1a3e5fc
Warren Weckesser <warren.weckesser@gmail.com>
2025-09-21 17:10:12 -0400

    MAINT: Tweak random_spaced_demo.py

--- examples/random_spaced_demo.py
+++ examples/random_spaced_demo.py
@@ -1,3 +1,7 @@
+"""
+Demonstration of numerand.random_spaced().
+"""
+
 # To plot the theoretical PDFs of the marginal distributions, this
 # script requires scipy.
 
@@ -19,8 +23,7 @@ for k in range(s.shape[1]):
     ax.hist(s[:, k], bins=100, density=True, alpha=0.3)
 ax.set_title(f"Normalized histograms for components of\n"
              f"random_spaced(low={low}, high={high}, delta={delta}, "
-             f"n={n}, size={size})\n"
-             "Dashed curves are the marginal PDFs")
+             f"n={n}, size={size})")
 ax.grid(alpha=0.2)
 
 # Plot the PDFs of the marginal distributions of each component.
@@ -32,12 +35,14 @@ if (n-1)*delta < high - low:
         dist = beta(k + 1, n - k, loc=left, scale=right - left)
         xx = np.linspace(left, right, 400)
         yy = dist.pdf(xx)
-        ax.plot(xx, yy, 'k--', linewidth=1, alpha=0.5)
+        kwargs = {'label': 'marginal PDF'} if k == 0 else {}
+        ax.plot(xx, yy, 'k--', linewidth=1, alpha=0.5, **kwargs)
         if n > 1:
             # Mark the mode with a dot.
             mode0 = k/(n-1)
             mode = (right-left)*mode0 + left
             ax.plot(mode, dist.pdf(mode), 'k.', alpha=0.25)
 
+ax.legend(shadow=True, framealpha=1)
 fig.tight_layout()
 plt.show()
