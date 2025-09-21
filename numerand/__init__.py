cf8e90ad2e0b069e2813429920527b4c5d4ff27b
Warren Weckesser <warren.weckesser@gmail.com>
2025-09-21 16:45:16 -0400

    MAINT: Use pyproject.toml

--- numerand/__init__.py
+++ numerand/__init__.py
@@ -1,7 +1,9 @@
-
-__version__ = "0.0.1.dev8"
+import importlib.metadata
 
 from ._random_table import random_table, random_table_from_table
 from ._select import select
 from ._uniform_simplex import uniform_simplex, uniform_simplices
 from ._random_spaced import random_spaced
+
+
+__version__ = importlib.metadata.version("numerand")
